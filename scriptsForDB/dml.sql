fc7ab63ebe823d9efff7cc6a7dc600b5b97f05fb
CryingArizonaTears <ivarti12@gmail.com>
2022-04-27 10:57:32 +0300

    Docker files

--- scriptsForDB/dml.sql
+++ scriptsForDB/dml.sql
@@ -1,4 +1,4 @@
-INSERT INTO employee (first_name, last_name, department_id, job_title, gender, date_of_birth) VALUES
+ INSERT INTO employee (first_name, last_name, department_id, job_title, gender, date_of_birth) VALUES
 ('test1', 'test1', 1, 'test1', 'MALE', '2000-11-11'),
 ('test2', 'test2', 2, 'test2', 'FEMALE', '2000-11-12'),
 ('test3', 'test3', 3, 'test3', 'OTHER', '2000-11-13'),
